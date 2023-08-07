3a2c21781f71aa1a85675cf590737d5f97ab0d2c
Gowthamits <141577354+Gowthamits@users.noreply.github.com>
2023-08-08 01:29:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -261,7 +261,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔥Movies Group💐 ', url='https://t.me/+YzD_kSaE-l04NzY1}>') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔥Movies Group💐 ', url='https://t.me/+YzD_kSaE-l04NzY1') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     

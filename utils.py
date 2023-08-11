c8c77f8313f594b907f222975ef1df653efb7b9e
SMDpaid <140572996+SMDpaid@users.noreply.github.com>
2023-08-11 16:21:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -455,7 +455,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://MoneyKamalo.com/api'
+    url = f''
     params = {'api': SHORTNER_API,
               'url': link,
               }
